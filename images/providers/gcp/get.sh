23f96814a2fbd315e6cc2ea20ac338f7a967604a
Cyril Diagne <diagne.cyril@gmail.com>
2019-10-12 16:04:27 +0200

    GCP - `get` checks the command

--- images/providers/gcp/get.sh
+++ images/providers/gcp/get.sh
@@ -8,9 +8,20 @@ source $KUDA_CMD_DIR/.config.sh
 gcloud container clusters get-credentials $KUDA_GCP_CLUSTER_NAME
 
 function prop() {
-  echo -e "\e[1m$1 : \e[34m $2 \e[0m"
+  echo -e "-\e[1m $1 : \e[36m $2 \e[0m"
 }
 
-# Print the IP Adress of the cluster.
-APP_IP_ADDRESS=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-prop "IP" $APP_IP_ADDRESS
+function get_status() {
+  echo -e "\n\e[1mStatus:"
+  # Print the IP Adress of the cluster.
+  APP_IP_ADDRESS=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+  prop "IP" $APP_IP_ADDRESS
+}
+
+case $1 in
+status) get_status ;;
+*)
+  echo "ERROR: command $1 unknown."
+  exit
+  ;;
+esac
