d3efeeeb309dee93de656ee9f6ee1c6daac80467
Cyril Diagne <diagne.cyril@gmail.com>
2019-10-13 22:12:27 +0200

    Fix env name

--- providers/aws/delete.sh
+++ providers/aws/delete.sh
@@ -3,6 +3,6 @@
 source $KUDA_CMD_DIR/.config.sh
 
 eksctl delete cluster \
---name=$KUDA_CLUSTER_NAME \
---region=$KUDA_CLUSTER_REGION \
+--name=$KUDA_AWS_CLUSTER_NAME \
+--region=$KUDA_AWS_CLUSTER_REGION \
 --wait
