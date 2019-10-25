14d27b57efb7417a10f899c6c54c744b5e45b6fc
Cyril Diagne <diagne.cyril@gmail.com>
2019-10-26 00:23:01 +0200

    GCP - fix delete

--- providers/gcp/app_delete.sh
+++ providers/gcp/app_delete.sh
@@ -9,11 +9,11 @@ app_version=$(echo $1 | cut -f2 -d':')
 app_image="gcr.io/$KUDA_GCP_PROJECT_ID/$app_name:$app_version"
 echo $app_image
 
-# Delete image from the repository.
-# gcloud container images delete $app_image
-
 # Get cluster's credentials to use kubectl.
 gcloud container clusters get-credentials $KUDA_GCP_CLUSTER_NAME
 
-# Launch.
+# Delete Knative service.
 kubectl delete -n kuda-app ksvc $app_name
+
+# Delete image from the repository.
+gcloud container images delete $app_image
