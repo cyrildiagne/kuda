20ec0295ba8dbd15e62e703507edd765ac298756
Cyril Diagne <diagne.cyril@gmail.com>
2019-12-31 07:10:38 +0100

    improve release script

--- hack/release.sh
+++ hack/release.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-export VERSION=0.3.1
+
+if [ -z "$1" ]; then
+  echo "ERROR: Version missing"
+  echo "Example usage: ./hack/release.sh 0.3.1"
+  exit 1
+fi
+
+VERSION=$1
+echo "Releasing v$VERSION..."
 
 # Tidy.
 go mod tidy
@@ -9,6 +17,6 @@ git tag v$VERSION-preview -m "Preview release."
 git push origin v$VERSION-preview
 
 # Master.
-git checkout master
-git merge v$VERSION-preview
-git push
+# git checkout master
+# git merge v$VERSION-preview
+# git push
